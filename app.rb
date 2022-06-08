dd4c6fb75c7069fd5cddf1b848e9826c06b37c23
Eghosa Edobor <eghosaedobor11@gmail.com>
2022-06-08 09:54:56 +0100

    Viewing Bookmarks

--- app.rb
+++ app.rb
@@ -1,15 +1,21 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require './lib/bookmark'
 
 class BookmarkManager < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
   end
 
-  get '/' do
-    'Bookmark Manager'
+  get '/bookmarks' do
+    @bookmarks = [
+              "http://www.makersacademy.com",
+              "http://www.destroyallsoftware.com",
+              "http://www.google.com"
+             ]
+  
+    erb :'bookmarks/index'
   end
 
   run! if app_file == $0
 end
-
